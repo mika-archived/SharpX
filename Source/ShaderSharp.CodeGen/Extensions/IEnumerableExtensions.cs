326d4f7cbe3e5dac228cd3ea6a2d2163fb214f8f
Natsuneko <me@mochizuki.moe>
2021-07-31 21:42:40 +0900

    feat: support to generate swizzle from generator

---
+++ Source/ShaderSharp.CodeGen/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShaderSharp.CodeGen.Extensions
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T[]> Combination<T>(this IEnumerable<T> items, int pick, bool withRepetition)
+        {
+            if (pick == 1)
+            {
+                foreach (var item in items) yield return new[] { item };
+
+                yield break;
+            }
+
+            foreach (var item in items)
+            {
+                var leftSide = new[] { item };
+                var remaining = withRepetition ? items : items.SkipWhile(w => !w.Equals(item)).Skip(1).ToList();
+
+                foreach (var rightSide in Combination(remaining, pick - 1, withRepetition)) yield return leftSide.Concat(rightSide).ToArray();
+            }
+        }
+    }
+}
---
+++ Source/ShaderSharp.CodeGen/Templates/ShaderSwizzleTemplate.Partial.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace ShaderSharp.CodeGen.Templates
+{
+    public partial class ShaderSwizzleTemplate
+    {
+        internal string ClassName { get; set; }
+
+        internal string[] Components { get; set; }
+
+        internal int MaxLength { get; set; }
+
+        private string GetReturnType(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return "T";
+
+                default:
+                    return $"Vector{i}Component<T>";
+            }
+        }
+
+        private string GetAccessors(string signature)
+        {
+            if (signature.Distinct().Count() != signature.Length)
+                return "{ get; }";
+            return "{ get; set; }";
+        }
+    }
+}
