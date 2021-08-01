b7ef03b8ee4ad82b5de30dc9824d1969727f5204
Natsuneko <me@mochizuki.moe>
2021-08-02 08:20:09 +0900

    fix: miss-tracking

---
+++ Source/ShaderSharp.Compiler.Abstractions/Attributes/PropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShaderSharp.Compiler.Abstractions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class PropertyAttribute : Attribute
+    {
+        public string Name { get; }
+
+        public PropertyAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
