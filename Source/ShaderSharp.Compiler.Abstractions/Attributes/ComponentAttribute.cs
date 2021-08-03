fd24e6fccc3edbcf1d7fc1259c14973c9d12c934
Natsuneko <me@mochizuki.moe>
2021-08-03 19:42:57 +0900

    chore: add validator to component

--- Source/ShaderSharp.Compiler.Abstractions/Attributes/ComponentAttribute.cs
+++ Source/ShaderSharp.Compiler.Abstractions/Attributes/ComponentAttribute.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ShaderSharp.Compiler.Abstractions.Attributes
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct)]
     public class ComponentAttribute : Attribute
     {
+        private static readonly Regex ValidComponentNameRegex = new("^[a-zA-Z][a-zA-Z0-9]+$", RegexOptions.Compiled);
         public string Name { get; }
 
         public ComponentAttribute(string name)
         {
             Name = name;
+        }
+
+        public bool IsValidName()
+        {
+            return ValidComponentNameRegex.IsMatch(Name);
         }
     }
 }
