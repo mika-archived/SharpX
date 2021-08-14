982af9b87e55a1d0462be679a5d0ba1bef50edcd
Natsuneko <me@mochizuki.moe>
2021-08-14 10:01:17 +0900

    feat: add naming function

---
+++ Source/SharpX.Compiler.ShaderLab/Models/HLSL/Naming.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace SharpX.Compiler.ShaderLab.Models.HLSL
+{
+    internal static class Naming
+    {
+        private static readonly Dictionary<string, int> Namings = new();
+
+        public static string GetSafeName(string candidateName)
+        {
+            if (Namings.ContainsKey(candidateName))
+            {
+                Namings[candidateName]++;
+                return $"{candidateName}_{Namings[candidateName]}";
+            }
+
+            Namings.Add(candidateName, 0);
+            return candidateName;
+        }
+    }
+}
