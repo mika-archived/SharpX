f3b28dbc058b3203ee2ea06bbf58acb55687657d
Natsuneko <me@mochizuki.moe>
2021-08-20 23:59:46 +0900

    feat(extensions): support type checking in `AsAttributeInstance`

--- Source/SharpX.Compiler.Extensions/AttributeDataExtensions.cs
+++ Source/SharpX.Compiler.Extensions/AttributeDataExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -8,19 +9,89 @@ namespace SharpX.Compiler.Extensions
 {
     public static class AttributeDataExtensions
     {
+        private static readonly Dictionary<string, string> TypeMappings = new()
+        {
+            { "void", typeof(void).FullName! },
+            { "string", typeof(string).FullName! },
+            { "int", typeof(int).FullName! },
+            { "uint", typeof(uint).FullName! },
+            { "long", typeof(long).FullName! },
+            { "ulong", typeof(ulong).FullName! },
+            { "short", typeof(short).FullName! },
+            { "ushort", typeof(ushort).FullName! },
+            { "char", typeof(char).FullName! },
+            { "bool", typeof(bool).FullName! },
+            { "byte", typeof(byte).FullName! },
+            { "sbyte", typeof(sbyte).FullName! },
+            { "float", typeof(float).FullName! },
+            { "double", typeof(double).FullName! },
+            { "decimal", typeof(decimal).FullName! },
+            { "object", typeof(object).FullName! }
+        };
+
+        private static string PrettyTypeName(string fullyQualifiedNameIncludeGlobal)
+        {
+            var fullyQualifiedName = fullyQualifiedNameIncludeGlobal.Replace("global::", "");
+            if (TypeMappings.ContainsKey(fullyQualifiedName))
+                return TypeMappings[fullyQualifiedName];
+            return fullyQualifiedName;
+        }
+
+        private static object ConvertType(TypedConstant c)
+        {
+            if (c.Kind == TypedConstantKind.Array)
+                return c.Values.Select(w => w.Value);
+            if (c.Kind == TypedConstantKind.Type)
+                return ((INamedTypeSymbol)c.Value!).ToDisplayString();
+
+            return c.Value!;
+        }
+
         public static T? AsAttributeInstance<T>(this AttributeData obj) where T : Attribute
         {
             var t = typeof(T);
+            return AsAttributeInstance(obj, t) as T;
+        }
+
+        public static object? AsAttributeInstance(this AttributeData obj, Type t)
+        {
+            if (!t.IsAssignableTo(typeof(Attribute)))
+                return default;
+
             var constructors = t.GetConstructors();
             var constructorArguments = obj.ConstructorArguments;
 
             // TODO: Type Checking
-            var constructor = constructors.FirstOrDefault(w => w.GetParameters().Length == constructorArguments.Length);
+            var candidates = constructors.Where(w => w.GetParameters().Length == constructorArguments.Length).ToList();
+            if (candidates.Count == 0)
+                return default;
+
+            var argumentValues = constructorArguments.Select(ConvertType).ToArray();
+            var argumentTyping = constructorArguments.Select(w => PrettyTypeName(w.Type!.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat))).ToArray();
+            var constructor = candidates.FirstOrDefault(w =>
+            {
+                foreach (var (parameter, index) in w.GetParameters().Select((v, i) => (v, i)))
+                {
+                    if (parameter.ParameterType.FullName == typeof(object).FullName)
+                        return true; // always true
+
+                    if (parameter.ParameterType.FullName == "System.String" && argumentTyping[index] == "System.Type")
+                        return true; // THIS IS A WORKAROUND FOR TypeConstantKind.Type.
+
+                    if (parameter.ParameterType.FullName == "System.Type" && argumentTyping[index] == "System.Type")
+                        return false; // THIS IS A WORKAROUND FOR TypeConstantKind.Type.
+
+                    if (parameter.ParameterType.FullName!.Replace("+", ".") != argumentTyping[index])
+                        return false;
+                }
+
+                return true;
+            });
 
             if (constructor == null)
                 return default;
 
-            var instance = (T) constructor.Invoke(constructorArguments.Select(w => w.Value).ToArray());
+            var instance = constructor.Invoke(argumentValues);
 
             if (obj.NamedArguments.Length == 0)
                 return instance;
