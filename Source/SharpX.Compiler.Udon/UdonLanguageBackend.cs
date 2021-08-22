f83a4facafea89d966d7e5fc15ff5f0ddb90a82d
Natsuneko <me@mochizuki.moe>
2021-08-23 07:56:24 +0900

    feat(udon): support to start udon

---
+++ Source/SharpX.Compiler.Udon/UdonLanguageBackend.cs
@@ -0,0 +1,17 @@
+﻿
+using SharpX.Compiler.Composition.Attributes;
+using SharpX.Compiler.Composition.Interfaces;
+
+namespace SharpX.Compiler.Udon;
+
+[LanguageBackend]
+public class UdonLanguageBackend : ILanguageBackend
+{
+    public string Identifier => "Udon";
+
+    public void Initialize(ILanguageBackendContext context)
+    {
+        //
+        context.RegisterExtension("uasm");
+    }
+}
