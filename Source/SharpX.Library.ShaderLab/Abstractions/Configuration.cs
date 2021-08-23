c08458b74dc3716544731feead2f464955cbeca0
Natsuneko <me@mochizuki.moe>
2021-08-24 00:29:52 +0900

    feat(shaderlab): support to dynamic configuration for entry points

---
+++ Source/SharpX.Library.ShaderLab/Abstractions/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using SharpX.Library.ShaderLab.Attributes;
+
+namespace SharpX.Library.ShaderLab.Abstractions
+{
+    public static class Configuration
+    {
+        public enum EntryPoint
+        {
+            VertexShader,
+
+            GeometryShader,
+
+            FragmentShader
+        }
+
+        public static string GetShaderEntryPoint(Type t, EntryPoint e)
+        {
+            var methods = t.GetMethods();
+            switch (e)
+            {
+                case EntryPoint.VertexShader:
+                    return GetActualNameForMethod(methods.FirstOrDefault(w => w.GetCustomAttribute<VertexShaderAttribute>() != null));
+                case EntryPoint.GeometryShader:
+                    return GetActualNameForMethod(methods.FirstOrDefault(w => w.GetCustomAttribute<GeometryShaderAttribute>() != null));
+                case EntryPoint.FragmentShader:
+                    return GetActualNameForMethod(methods.FirstOrDefault(w => w.GetCustomAttribute<FragmentShaderAttribute>() != null));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(e), e, null);
+            }
+        }
+
+        private static string GetActualNameForMethod(MethodInfo m)
+        {
+            var attr = m.GetCustomAttribute<FunctionAttribute>();
+            if (attr != null)
+                return attr.Alternative;
+            return m.Name;
+        }
+    }
+}
