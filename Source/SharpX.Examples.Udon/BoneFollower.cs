b93e22910e501808fa710f27fc0a8f56d7d9cfa1
Natsuneko <me@mochizuki.moe>
2021-08-30 09:35:56 +0900

    chore(udon): add example project

---
+++ Source/SharpX.Examples.Udon/BoneFollower.cs
@@ -0,0 +1,33 @@
+﻿using SharpX.Library.Udon;
+
+using UnityEngine;
+
+using VRC.SDKBase;
+
+namespace SharpX.Examples.Udon
+{
+    [AddComponentMenu("SharpX.Udon/Utilities/Bone Follower")]
+    public class BoneFollower : SharpXUdonBehaviour
+    {
+        private bool isInEditor;
+
+        private VRCPlayerApi playerApi;
+
+        [SerializeField]
+        private HumanBodyBones trackedBone;
+
+        private void Start()
+        {
+            playerApi = Networking.LocalPlayer;
+            isInEditor = playerApi == null;
+        }
+
+        private void Update()
+        {
+            if (isInEditor)
+                return;
+
+            transform.SetPositionAndRotation(playerApi.GetBonePosition(trackedBone), playerApi.GetBoneRotation(trackedBone));
+        }
+    }
+}
---
+++ Source/SharpX.Examples.Udon/InteractToggle.cs
@@ -0,0 +1,22 @@
+﻿using SharpX.Library.Udon;
+using SharpX.Library.Udon.Attributes;
+using SharpX.Library.Udon.Enums;
+
+using UnityEngine;
+
+namespace SharpX.Examples.Udon
+{
+    [AddComponentMenu("Udon Sharp/Utilities/Interact Toggle")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class InteractToggle : SharpXUdonBehaviour
+    {
+        [Tooltip("List of objects to toggle on and off")]
+        public GameObject[] toggleObjects;
+
+        public override void Interact()
+        {
+            foreach (var toggleObject in toggleObjects)
+                toggleObject.SetActive(!toggleObject.activeSelf);
+        }
+    }
+}
