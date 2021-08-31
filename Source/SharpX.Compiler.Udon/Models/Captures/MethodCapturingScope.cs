85de233421fdf5933ac4d1388f4c2125d7e776bb
Natsuneko <me@mochizuki.moe>
2021-09-01 08:23:50 +0900

    feat(udon): support to nested method (prepare)

---
+++ Source/SharpX.Compiler.Udon/Models/Captures/MethodCapturingScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using SharpX.Compiler.Udon.Models.Symbols;
+
+namespace SharpX.Compiler.Udon.Models.Captures
+{
+    internal class MethodCapturingScope : IDisposable
+    {
+        private readonly UdonSymbolEnclosure _enclosure;
+        private readonly UdonCSharpSyntaxWalker _walker;
+
+        public MethodCapturingScope(UdonCSharpSyntaxWalker walker, MethodDeclarationSyntax node)
+        {
+            _walker = walker;
+            _walker.MethodCapturingStack.Push(UdonNodeResolver.Instance.RemappedBuiltinEvent(node.Identifier.ValueText));
+            _enclosure = new UdonSymbolEnclosure(walker);
+        }
+
+
+        public void Dispose()
+        {
+            _walker.MethodCapturingStack.Pop();
+            _enclosure.Dispose();
+        }
+    }
+}
---
+++ Source/SharpX.Compiler.Udon/Models/Symbols/UdonSymbolEnclosure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SharpX.Compiler.Udon.Models.Symbols
+{
+    internal class UdonSymbolEnclosure : IDisposable
+    {
+        private readonly UdonCSharpSyntaxWalker _walker;
+
+        public UdonSymbolEnclosure(UdonCSharpSyntaxWalker walker)
+        {
+            _walker = walker;
+            _walker.SymbolTable?.OpenSymbolTable();
+        }
+
+        public void Dispose()
+        {
+            _walker.SymbolTable?.CloseSymbolTable();
+        }
+    }
+}
