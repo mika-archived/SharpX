c6bb212baaf91d7485fa7a7b11a8f216500ba730
Natsuneko <me@mochizuki.moe>
2021-08-31 21:10:48 +0900

    feat(composition): add safe stack

---
+++ Source/SharpX.Compiler.Composition/Abstractions/SafeStack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SharpX.Compiler.Composition.Abstractions
+{
+    public class SafeStack<T>
+    {
+        private readonly Stack<T> _stack;
+
+        public int Count => _stack.Count;
+
+        public SafeStack()
+        {
+            _stack = new Stack<T>();
+        }
+
+        public SafeStack(SafeStack<T> innerStack)
+        {
+            _stack = new Stack<T>(innerStack._stack);
+        }
+
+        [return: NotNullIfNotNull("default")]
+        public T? SafePeek(T? @default = default)
+        {
+            return _stack.Count > 0 ? _stack.Peek() : @default;
+        }
+
+        public T Peek()
+        {
+            return _stack.Peek();
+        }
+
+        public void Push(T data)
+        {
+            _stack.Push(data);
+        }
+
+        public T Pop()
+        {
+            return _stack.Pop();
+        }
+
+        public bool Contains(T data)
+        {
+            return _stack.Contains(data);
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
